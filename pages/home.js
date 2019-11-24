801593947bc1654dc8a3ec0140eb3f8f0cc7a49f
Jonathan Bueff <jonathanbueff@Jonathans-MacBook-Pro.local>
2019-11-24 01:37:03 -0600

    debugging playlists

--- pages/home.js
+++ pages/home.js
@@ -19,7 +19,7 @@ Home.getInitialProps = async function(req){
   let code = req.query.code;
   let playlists;
   const res = await fetch(awsinstance + ':3456/getPlaylists?code='+code);
-  console.log(res);
+  console.log(res.json());
 
   // console.log(JSON.stringify(res.data));
   return{
