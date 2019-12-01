882a8279c9322a0855c03db8a6d34f0ebc19b5fd
Jonathan Bueff <jonathanbueff@Jonathans-MBP.attlocal.net>
2019-12-01 13:33:45 -0800

    debug

--- comps/profile.js
+++ comps/profile.js
@@ -28,11 +28,14 @@ const Profile = (props) => (
       <div className="playlistContainer">
         <h4 className="playlistListTitle">Playlists</h4>
         <ol className="playlist">
+        props.data.allPlaylists.forEach(element => {
           <li className = "playlistListItem"><Playlist {...props.data.allPlaylists[0]}/> </li>
+        });
+          {/* <li className = "playlistListItem"><Playlist {...props.data.allPlaylists[0]}/> </li>
           <li className = "playlistListItem"><Playlist></Playlist></li>
           <li className = "playlistListItem"><Playlist></Playlist></li>
           <li className = "playlistListItem"><Playlist></Playlist></li>
-          <li className = "playlistListItem"><Playlist></Playlist></li>
+          <li className = "playlistListItem"><Playlist></Playlist></li> */}
         </ol>
       </div>
       <style jsx>{`
