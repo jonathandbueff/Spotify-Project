8757f039b0233adc2461a245aaf2634faa54464c
Jonathan Bueff <jonathanbueff@Jonathans-MBP.attlocal.net>
2019-12-01 22:52:16 -0800

    check to see if dataAll workin

--- pages/home.js
+++ pages/home.js
@@ -98,7 +98,7 @@ Home.getInitialProps = async function(req){
   const result = await fetch(awsinstance+':3456/getData?token='+accessToken+'&username='+username);
   const dataAll = await result.json();
   
-  console.log(dataAll[0].playlists[0].title);
+  console.log(dataAll[0].playlists[0].titdddle);
   // const result2 = await fetch(awsinstance+':3456/getOtherUsers?token='+accessToken+'&username='+username);
   // const otherUsers = await result2.json();
   // console.log(otherUsers);
