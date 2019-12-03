d73184555367e07b875fe81fa02eef0045b6135b
Jonathan Bueff <jonathanbueff@Jonathans-MBP.attlocal.net>
2019-12-02 18:10:16 -0800

    idk

--- server/server.js
+++ server/server.js
@@ -227,12 +227,11 @@ async function insertDataHelper(jsonToken) {
   playlists_parsed.forEach(async playlist => {
     let tracks_JSON = await getPlaylistTracks(playlist.href, accessToken);
     let tracksInPlaylist = await listOfTracks(tracks_JSON);
-    console.log(tracksInPlaylist);
     let playlistName = playlist.title;
-    let sqlPlaylist ="insert INTO playlist (playlist, tracks) VALUES ('" + playlistName + "','" + tracks_JSON +"') ON DUPLICATE KEY UPDATE playlist = '" + playlistName + "', tracks = '" + tracks_JSON +"'";
-    // con.query(sqlPlaylist, function (err, result) {
-    //   if (err) console.log(err);
-    // });
+    let sqlPlaylist ="insert INTO playlist (playlist, tracks) VALUES ('" + playlistName + "','" + tracksInPlaylist +"') ON DUPLICATE KEY UPDATE playlist = '" + playlistName + "', tracks = '" + tracksInPlaylist +"'";
+    con.query(sqlPlaylist, function (err, result) {
+      if (err) console.log(err);
+    });
   })
 
 
