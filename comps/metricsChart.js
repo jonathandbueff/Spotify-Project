8afa912ba73149ecca769490c701bfacb5b7532f
Jonathan Bueff <jonathanbueff@Jonathans-MacBook-Pro.local>
2019-12-03 23:04:30 -0600

    changed aws

--- comps/metricsChart.js
+++ comps/metricsChart.js
@@ -75,7 +75,7 @@ const MetricsChart = (props) => {
             fontSize: 18
           },
           angleLines: {
-            display: false,
+            display: true,
             color: 'white'
         },
         }
