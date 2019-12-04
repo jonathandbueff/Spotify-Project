c1d907007a509698b3cf787a6ce5738e177378f7
Jonathan Bueff <jonathanbueff@Jonathans-MacBook-Pro.local>
2019-12-03 19:49:08 -0600

    changed aws

--- pages/playlistDisplay.js
+++ pages/playlistDisplay.js
@@ -9,8 +9,9 @@ import SideBar from '../comps/sideBar';
 import Footer from '../comps/footer';
 import PlaylistPage from '../comps/playlistPage';
 import MetricsChart from '../comps/metricsChart';
-// let awsinstance = 'http://ec2-18-191-11-49.us-east-2.compute.amazonaws.com'; //Jon
-let awsinstance = 'http://ec2-18-234-109-238.compute-1.amazonaws.com'; //Joe
+let awsinstance = 'http://ec2-18-191-11-49.us-east-2.compute.amazonaws.com'; //Jon
+// let awsinstance = 'http://ec2-18-234-109-238.compute-1.amazonaws.com'; //Joe
+
 const homeStyle ={
   position: "absolute",
   background: "url('/12.png') repeat center fixed",
